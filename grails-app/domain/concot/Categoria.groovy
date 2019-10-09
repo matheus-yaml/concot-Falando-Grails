cef15ae13a5a387a5283586c67361a97a04be1fe
Your Name <you@example.com>
2019-10-09 06:22:22 -0300

    commit

--- grails-app/domain/concot/Categoria.groovy
+++ grails-app/domain/concot/Categoria.groovy
@@ -5,6 +5,6 @@ class Categoria {
     String nome
 
     static constraints = {
-	nome nullable: false, blank, false, maxsize: 128, unique: true
+	nome nullable: false, blank: false, maxsize: 128, unique: true
     }
 }
